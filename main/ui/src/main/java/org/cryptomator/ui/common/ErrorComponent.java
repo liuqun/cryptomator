812593f73a29ac74fbe6438d32b9af63e1793d21
JaniruTEC <JaniruTEC@gmail.com>
2020-10-19 19:16:43 +0200

    Added support for logging to ErrorComponent, added convenience methods

--- main/ui/src/main/java/org/cryptomator/ui/common/ErrorComponent.java
+++ main/ui/src/main/java/org/cryptomator/ui/common/ErrorComponent.java
@@ -1,24 +1,56 @@
 package org.cryptomator.ui.common;
 
+import com.google.common.base.Strings;
 import dagger.BindsInstance;
 import dagger.Subcomponent;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 
 @Subcomponent(modules = {ErrorModule.class})
 public interface ErrorComponent {
 
+	Throwable cause();
+
 	Stage window();
+
+	@Nullable
+	String message();
+
+	@Nullable
+	Boolean suppressErrorLogging();
 
 	@FxmlScene(FxmlFile.ERROR)
 	Scene scene();
 
+	default void reportToUser() {
+		Logger logger = LoggerFactory.getLogger(ErrorComponent.class); //TODO Caller Sensitive?
+		String message = Strings.isNullOrEmpty(message()) ? "An unexpected error occured" : message();
+
+		Boolean suppressErrorLogging = suppressErrorLogging();
+		if (suppressErrorLogging == null || !suppressErrorLogging) {
+			logger.error(message, cause());
+		} else {
+			logger.error(message);
+		}
+		showOnFXThread();
+	}
+
 	default void showErrorScene() {
+		if (!Platform.isFxApplicationThread()) {
+			throw new IllegalStateException("Must be run on FX Application Thread");
+		}
 		Stage stage = window();
 		stage.setScene(scene());
 		stage.show();
+	}
+
+	default void showOnFXThread() {
+		Platform.runLater(this::showErrorScene);
 	}
 
 	@Subcomponent.Builder
@@ -32,6 +64,12 @@ public interface ErrorComponent {
 
 		@BindsInstance
 		Builder returnToScene(@Nullable Scene previousScene);
+
+		@BindsInstance
+		Builder message(@Nullable String message);
+
+		@BindsInstance
+		Builder suppressErrorLogging(@Nullable Boolean suppressErrorLogging);
 
 		ErrorComponent build();
 
